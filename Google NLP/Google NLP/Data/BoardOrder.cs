2461d05012aa246a5275fe952240ab8b6b233f66
Daniel Sandoval <dsandoval@fsmb.org>
2019-02-08 14:22:19 -0600

    Add google nlp project.

---
+++ Google NLP/Google NLP/Data/BoardOrder.cs
@@ -0,0 +1,50 @@
+﻿using Google.Cloud.Language.V1;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace GoogleNLPConsole.Data
+{
+    public class BoardOrder
+    {
+        public BoardOrder() { }
+        public BoardOrder( string responseJson )
+        {
+            Response = JsonConvert.DeserializeObject<AnnotateTextResponse>(responseJson);
+            SetResponse(Response);
+        }
+
+        public AnnotateTextResponse Response { get; set; }
+        public string ResponseJson { get; set; }
+
+        public string Path { get; set; }
+        public string Text { get; set; }
+        public string Categories { get; set; }
+        public string DocumentSentiment { get; set; }
+        public string Entities { get; set; }
+        public string Sentences { get; set; }
+        public string Token { get; set; }
+
+        public void SetResponse(AnnotateTextResponse response)
+        {
+            Categories = response.Categories != null ? JsonConvert.SerializeObject(response.Categories, Formatting.None) : null;
+            DocumentSentiment = response.DocumentSentiment != null ? JsonConvert.SerializeObject(response.DocumentSentiment, Formatting.None) : null;
+            Entities = response.Entities != null ? JsonConvert.SerializeObject(response.Entities, Formatting.None) : null;
+            Sentences = response.Sentences != null ? JsonConvert.SerializeObject(response.Sentences, Formatting.None) : null;
+            Token = response.Tokens != null ? JsonConvert.SerializeObject(response.Tokens, Formatting.None) : null;
+            Response = response.Clone();
+            ResponseJson = response != null ? JsonConvert.SerializeObject(Response, Formatting.None) : null;
+        }
+    }
+
+    public static class Extensions
+    {
+        public static IEnumerable<string> SplitByLength(this string str, int maxLength)
+        {
+            for (int index = 0; index < str.Length; index += maxLength)
+            {
+                yield return str.Substring(index, Math.Min(maxLength, str.Length - index));
+            }
+        }
+    }
+}
---
+++ Google NLP/Google NLP/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using Google.Cloud.Language.V1;
+
+using GoogleNLPConsole.Data;
+using static Google.Cloud.Language.V1.AnnotateTextRequest.Types;
+
+namespace GoogleNLPConsole
+{
+    public class Program
+    {
+        private static readonly string _connString = @"";
+        private static readonly string _path = @"C:\output";
+
+        public static void Main(string[] args)
+        {
+            // Before running through Google NLP api, I stored the board order text in a database in the board order table referenced in the db calls
+
+            //Get order text and anazlyze
+            var orders = NLPDatabase.GetBoardOrdersText(_connString);
+
+            // Run through Google NLP api, save response in database in a Google table
+            AnalyzeEverything(orders);
+
+            ////Write sentences to output file
+            //var responses = NLPDatabase.GetResponses( _connString);
+            //responses = responses.Where(r => !r.Path.Contains("Sexual")).ToList();
+            //ExtractSentences(responses, _spath);
+
+            Console.Write("\nPress any key to continue...");
+            Console.ReadKey();
+        }
+
+        public static void AnalyzeEverything(List<BoardOrder> orders)
+        {
+            var client = LanguageServiceClient.Create();
+            var responses = new List<AnnotateTextResponse>();
+            var truncatedOrders = new List<BoardOrder>();
+
+            //Truncate orders with text exceeding the size limit.
+            foreach (var order in orders.ToList())
+            {
+                if (order.Text.Length > 60000)
+                {
+                    var splitText = order.Text.SplitByLength(60000);
+                    foreach (var text in splitText)
+                    {
+                        truncatedOrders.Add(new BoardOrder()
+                        {
+                            Path = order.Path,
+                            Text = text,
+                        });
+                    }
+                    orders.Remove(order);
+                    continue;
+                }
+            }
+
+            orders.AddRange(truncatedOrders);
+
+            // Get text from each order and run through the api
+            foreach (var order in orders)
+            {
+                var response = client.AnnotateText(new Document()
+                {
+                    //Removed new lines to get better output
+                    Content = order.Text.Replace("\r\n", " "),
+                    Type = Document.Types.Type.PlainText
+                }, new Features()
+                {
+                    ExtractSyntax = true,
+                    ExtractDocumentSentiment = true,
+                    ExtractEntities = true,
+                    ExtractEntitySentiment = true,
+                    ClassifyText = true
+                });
+
+                order.SetResponse(response);
+            }
+
+            // save response
+            NLPDatabase.SaveResponses(orders, _connString);
+
+            return;
+        }
+
+        // Given output from Google NLP, store sentences in txt file. 
+        public static void ExtractSentences(List<BoardOrder> responses, string path)
+        {
+            foreach (var response in responses)
+            {
+                var fileName = Path.GetFileName(response.Path);
+                var fullPath = response.Path.Replace(_path, path);
+                var filePath = response.Path.Replace(fileName, "").Replace(_path, path);
+                Directory.CreateDirectory(filePath);
+                using (var f = new StreamWriter(fullPath, true))
+                {
+                    foreach (var sentence in response.Response.Sentences)
+                    {
+                        f.WriteLine(sentence.Text.Content);
+                    }
+                }
+            }
+        }
+
+        // Given output from Google NLP, store tokens in txt file. 
+        public static void ExtractTokens(List<BoardOrder> responses, string path)
+        {
+            foreach (var response in responses)
+            {
+                var fileName = Path.GetFileName(response.Path);
+                var fullPath = response.Path.Replace(_path, path);
+                var filePath = response.Path.Replace(fileName, "").Replace(_path, path);
+                Directory.CreateDirectory(filePath);
+                using (var f = new StreamWriter(fullPath, true))
+                {
+                    foreach (var token in response.Response.Tokens)
+                    {
+                        f.WriteLine(token.Text.Content);
+                    }
+                }
+            }
+        }
+
+        // Given output from Google NLP, store sentences in txt file. 
+        public static void Sentences(List<BoardOrder> responses, string path)
+        {
+            using (var f = new StreamWriter(path))
+            {
+                foreach (var r in responses.Skip(5).Take(1))
+                {
+                    foreach (var s in r.Response.Sentences)
+                    {
+                        f.WriteLine(s.Text.Content.Replace("\r\n", ""));
+                    }
+                }
+            }
+        }
+
+        // Given output from Google NLP, store entities in txt file. 
+        public static void Entities(List<BoardOrder> responses, string path)
+        {
+            using (var f = new StreamWriter(path + "Entities.txt"))
+            {
+                foreach (var r in responses.Take(1))
+                {
+                    foreach (var e in r.Response.Entities)
+                        f.WriteLine(e.Name);
+                }
+            }
+        }
+
+    }
+}
